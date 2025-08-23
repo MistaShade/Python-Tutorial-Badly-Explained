0311b3dcdb8e602b15fa6a2593ba4b7d70f72a5c
Sam <114481646+MistaShade@users.noreply.github.com>
2025-08-23 16:57:21 +0800

    Add files via upload
    New Project :3

---
+++ Tutorial_Projects/ShoppingSpree.py
@@ -0,0 +1,11 @@
+# Input name
+# Pick a power up (Types of powerups? Will think about it)
+# Introduction
+# Introduction
+
+print("Welcome, to ShoppingSpree.")
+name = input("To continue, please eneter your name: ")
+input("You will face many crazy employees that will force you to buy their items.")
+input("Some items can help you survive")
+input("And of course, some items can put in a disadvantage.")
+input("Shop Well.")
